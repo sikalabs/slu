2e1ea820f0569a74e23a7346f6acbd51e9bdea26
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-03 12:27:30 +0200

    feat(cmd/root): Add optional JSON output (using --json)

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -4,7 +4,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var RootCmdFlagJson bool
+
 var RootCmd = &cobra.Command{
 	Use:   "slut",
 	Short: "SikaLabs Utils",
+}
+
+func init() {
+	RootCmd.PersistentFlags().BoolVar(
+		&RootCmdFlagJson,
+		"json",
+		false,
+		"Formatu output to JSON",
+	)
 }
