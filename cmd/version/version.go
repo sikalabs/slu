503e6de8075c9101534e791578628d75ab8d397f
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-03 17:43:38 +0200

    refactor: Move version to own package

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 
 	"github.com/sikalabs/slut/cmd/root"
+	"github.com/sikalabs/slut/version"
 	"github.com/spf13/cobra"
 )
-
-var version string = "v0.6.0-dev"
 
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints version",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		fmt.Printf("%s\n", version)
+		fmt.Printf("%s\n", version.Version)
 	},
 }
 
