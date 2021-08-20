7c6b26284d284eac98aa9b7c467cc6443fddbc02
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-20 23:08:43 +0200

    feat(cmd/version): Add verbose output (os, arch) of version command

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -2,11 +2,14 @@ package version
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/sikalabs/slu/cmd/root"
 	"github.com/sikalabs/slu/version"
 	"github.com/spf13/cobra"
 )
+
+var CmdFlagVerbose bool
 
 var Cmd = &cobra.Command{
 	Use:     "version",
@@ -14,10 +17,21 @@ var Cmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Args:    cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		fmt.Printf("%s\n", version.Version)
+		if CmdFlagVerbose {
+			fmt.Printf("%s %s %s\n", version.Version, runtime.GOOS, runtime.GOARCH)
+		} else {
+			fmt.Printf("%s\n", version.Version)
+		}
 	},
 }
 
 func init() {
 	root.RootCmd.AddCommand(Cmd)
+	Cmd.Flags().BoolVarP(
+		&CmdFlagVerbose,
+		"verbose",
+		"v",
+		false,
+		"Verbose version output",
+	)
 }
