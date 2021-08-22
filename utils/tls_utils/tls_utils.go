772c0ef74d951d31db8480cc73c7497947c3c29e
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-23 01:14:24 +0200

    feat(utils/tls_utils): Add PrintCertificateFromFile

--- utils/tls_utils/tls_utils.go
+++ utils/tls_utils/tls_utils.go
@@ -2,6 +2,7 @@ package tls_utils
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"time"
@@ -27,6 +28,26 @@ func PrintCertificateFromServer(addr string, serverName string) {
 	}
 	defer conn.Close()
 	certs := conn.ConnectionState().PeerCertificates
+	for _, cert := range certs {
+		fmt.Printf("Subject Name: %s\n", cert.Subject)
+		fmt.Printf("Subject Common Name: %s\n", cert.Subject.CommonName)
+		fmt.Printf("Issuer Name: %s\n", cert.Issuer)
+		fmt.Printf("Issuer Common Name: %s \n", cert.Issuer.CommonName)
+		fmt.Printf("Created: %s \n", cert.NotBefore.Format(time.RFC3339))
+		fmt.Printf("Expiry: %s \n", cert.NotAfter.Format(time.RFC3339))
+		fmt.Println()
+	}
+}
+
+func PrintCertificateFromFile(certFile, keyFile string) {
+	tlsCert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	certs, err := x509.ParseCertificates(tlsCert.Certificate[0])
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, cert := range certs {
 		fmt.Printf("Subject Name: %s\n", cert.Subject)
 		fmt.Printf("Subject Common Name: %s\n", cert.Subject.CommonName)
