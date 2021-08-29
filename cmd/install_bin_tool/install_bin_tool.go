e4370b66e81408e5e93dfa3621d5208bf992cd58
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-29 18:24:24 +0200

    feat(cmd/install_bin_tool): Add params for os and arch & refactor

--- cmd/install_bin_tool/install_bin_tool.go
+++ cmd/install_bin_tool/install_bin_tool.go
@@ -9,10 +9,23 @@ import (
 )
 
 var CmdFlagBinDir string
+var CmdFlagOS string
+var CmdFlagArch string
 
 var Cmd = &cobra.Command{
 	Use:   "install-bin-tool",
 	Short: "Install preconfigured binary tool like Terraform, Vault, ...",
+}
+
+func installHashicorpBin(tool, version string) {
+	url := "https://releases.hashicorp.com/" + tool + "/" +
+		version + "/terraform_" + version + "_" + CmdFlagOS + "_" + CmdFlagArch + ".zip"
+	install_bin_utils.InstallBin(
+		url,
+		"terraform",
+		CmdFlagBinDir,
+		"terraform",
+	)
 }
 
 var InstallTerraformCmd = &cobra.Command{
@@ -20,16 +33,7 @@ var InstallTerraformCmd = &cobra.Command{
 	Short: "Install terraform binary",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		version := "1.0.5"
-		arch := runtime.GOOS
-		url := "https://releases.hashicorp.com/terraform/" +
-			version + "/terraform_" + version + "_" + arch + "_amd64.zip"
-		install_bin_utils.InstallBin(
-			url,
-			"terraform",
-			CmdFlagBinDir,
-			"terraform",
-		)
+		installHashicorpBin("terraform", "1.0.5")
 	},
 }
 
@@ -38,16 +42,7 @@ var VaultTerraformCmd = &cobra.Command{
 	Short: "Install vault binary",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		version := "1.8.2"
-		arch := runtime.GOOS
-		url := "https://releases.hashicorp.com/vault/" +
-			version + "/vault_" + version + "_" + arch + "_amd64.zip"
-		install_bin_utils.InstallBin(
-			url,
-			"vault",
-			CmdFlagBinDir,
-			"vault",
-		)
+		installHashicorpBin("vault", "1.8.2")
 	},
 }
 
@@ -59,6 +54,20 @@ func init() {
 		"d",
 		"/usr/local/bin",
 		"Binary dir",
+	)
+	Cmd.PersistentFlags().StringVarP(
+		&CmdFlagOS,
+		"os",
+		"o",
+		runtime.GOOS,
+		"OS",
+	)
+	Cmd.PersistentFlags().StringVarP(
+		&CmdFlagArch,
+		"arch",
+		"a",
+		runtime.GOARCH,
+		"Architecture",
 	)
 	Cmd.AddCommand(InstallTerraformCmd)
 	Cmd.AddCommand(VaultTerraformCmd)
