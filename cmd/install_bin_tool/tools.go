0427a7e5cebc503cc5c37ef9efcc32034864410a
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-29 22:59:06 +0200

    feat(cmd/install_bin_tool): Add kubectl to config

--- cmd/install_bin_tool/tools.go
+++ cmd/install_bin_tool/tools.go
@@ -2,6 +2,12 @@ package install_bin_tool
 
 var Tools = []Tool{
 	{
+		Name: "kubectl",
+		// Get Version: `curl -L -s https://dl.k8s.io/release/stable.txt`
+		Version:     "v1.22.1",
+		UrlTemplate: "https://dl.k8s.io/release/{{.Version}}/bin/{{.Os}}/{{.Arch}}/kubectl",
+	},
+	{
 		Name:        "terraform",
 		Version:     "1.0.5",
 		UrlTemplate: hashicorpUrlTemplate("terraform"),
