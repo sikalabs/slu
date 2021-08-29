a3c8938706f220052c2588a485aa9a50528aad7f
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-29 16:54:04 +0200

    feat(utils/zip_utils): Add WebZipToBin util

---
+++ utils/zip_utils/zip_utils.go
@@ -0,0 +1,50 @@
+package zip_utils
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func WebZipToBin(url, inZipFileName, outFileName string) {
+	var err error
+
+	resp, err := http.Get(url)
+	handleError(err)
+	defer resp.Body.Close()
+
+	tmpInFile, err := os.CreateTemp("", "go-zip-example")
+	handleError(err)
+
+	_, err = io.Copy(tmpInFile, resp.Body)
+	handleError(err)
+
+	r, err := zip.OpenReader(tmpInFile.Name())
+	handleError(err)
+	defer r.Close()
+
+	for _, f := range r.File {
+		if f.Name == inZipFileName {
+			outFile, err := os.OpenFile(outFileName, os.O_CREATE|os.O_WRONLY, 755)
+			handleError(err)
+			defer outFile.Close()
+
+			zipFile, _ := f.Open()
+			defer zipFile.Close()
+
+			_, err = io.Copy(outFile, zipFile)
+			handleError(err)
+			return
+		}
+	}
+	handleError(fmt.Errorf("file \"%s\" not found in ZIP", inZipFileName))
+}
