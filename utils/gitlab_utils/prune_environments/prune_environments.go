b91a16c952e8dfa8ccaf1ccfa73ea834e3f96e13
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-16 12:12:31 +0100

    feat(utils/gitlab_utils): Add prune environment util

---
+++ utils/gitlab_utils/prune_environments/prune_environments.go
@@ -0,0 +1,71 @@
+package prune_environments
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/xanzy/go-gitlab"
+)
+
+type Config struct {
+	ApiUrl    string
+	Token     string
+	ProjectID int
+}
+
+func LoadConfig(config *Config, path string) error {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+func PruneEnvironments(path string) {
+	var config Config
+	err := LoadConfig(&config, path)
+	if err != nil {
+		panic(err)
+	}
+	g, err := gitlab.NewClient(
+		config.Token,
+		gitlab.WithBaseURL(config.ApiUrl),
+	)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		envs, _, err := g.Environments.ListEnvironments(
+			config.ProjectID,
+			&gitlab.ListEnvironmentsOptions{},
+		)
+		if err != nil {
+			panic(err)
+		}
+		if len(envs) == 0 {
+			break
+		}
+		for _, e := range envs {
+			fmt.Println("stop & delete environment:", e.Name)
+			_, err = g.Environments.StopEnvironment(config.ProjectID, e.ID)
+			if err != nil {
+				panic(err)
+			}
+			_, err = g.Environments.DeleteEnvironment(config.ProjectID, e.ID)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
