112ab378168792a639ad13223060254c43736e1e
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-16 12:31:34 +0100

    feat(cmd/time/prefix): Add no since, no timestamp and separator flags

--- cmd/time/prefix/prefix.go
+++ cmd/time/prefix/prefix.go
@@ -12,6 +12,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var CmdFlagNoSince bool
+var CmdFlagNoTimestamp bool
+var CmdFlagSeparator bool
+
 var Cmd = &cobra.Command{
 	Use:   "prefix",
 	Short: "Prefix stdin with timestap",
@@ -21,11 +25,41 @@ var Cmd = &cobra.Command{
 		s := bufio.NewScanner(os.Stdin)
 		for s.Scan() {
 			since := time.Since(start)
-			fmt.Println(time_utils.DurationToString(since), s.Text())
+			if !CmdFlagNoTimestamp {
+				fmt.Printf("%s ", time.Now().Format("2006-01-02 15:04:05"))
+			}
+			if !CmdFlagNoSince {
+				fmt.Printf("%s ", time_utils.DurationToString(since))
+			}
+			if CmdFlagSeparator {
+				fmt.Print("| ")
+			}
+			fmt.Println(s.Text())
 		}
 	},
 }
 
 func init() {
 	time_cmd.Cmd.AddCommand(Cmd)
+	Cmd.PersistentFlags().BoolVarP(
+		&CmdFlagNoSince,
+		"no-since",
+		"S",
+		false,
+		"No since time (from start)",
+	)
+	Cmd.PersistentFlags().BoolVarP(
+		&CmdFlagNoTimestamp,
+		"no-timestamp",
+		"T",
+		false,
+		"No timestamp",
+	)
+	Cmd.PersistentFlags().BoolVarP(
+		&CmdFlagSeparator,
+		"separator",
+		"s",
+		false,
+		"Add separator (\" | \")",
+	)
 }
