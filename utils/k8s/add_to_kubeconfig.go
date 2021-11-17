860c3164351bc201e8270b2d75864f8aba476c0e
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-17 21:09:42 +0100

    feat(utils/k8s): Add AddToKubeconfig (shell implementation)

---
+++ utils/k8s/add_to_kubeconfig.go
@@ -0,0 +1,10 @@
+package k8s
+
+import "github.com/sikalabs/slu/utils/exec_utils"
+
+func AddToKubeconfigShell(path string) error {
+	return exec_utils.ExecOut("sh", "-c", `
+	cp ~/.kube/config ~/.kube/.config.$(date +%Y-%m-%d_%H-%M-%S).backup
+	KUBECONFIG=`+path+`:~/.kube/config kubectl config view --raw > /tmp/kubeconfig.merge.yml && cp /tmp/kubeconfig.merge.yml ~/.kube/config
+	`)
+}
