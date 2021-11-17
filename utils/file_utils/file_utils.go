5e652b36bed64f13b6284d1e7ad5079e8d4a185a
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-17 21:29:04 +0100

    feat(utils/file_utils): Add file utils with RemoveLines method

---
+++ utils/file_utils/file_utils.go
@@ -0,0 +1,67 @@
+package file_utils
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// From https://www.rosettacode.org/wiki/Remove_lines_from_a_file#Go
+func RemoveLines(fn string, start, n int) (err error) {
+	if start < 1 {
+		return errors.New("invalid request.  line numbers start at 1")
+	}
+	if n < 0 {
+		return errors.New("invalid request.  negative number to remove")
+	}
+	var f *os.File
+	if f, err = os.OpenFile(fn, os.O_RDWR, 0); err != nil {
+		return
+	}
+	defer func() {
+		if cErr := f.Close(); err == nil {
+			err = cErr
+		}
+	}()
+	var b []byte
+	if b, err = ioutil.ReadAll(f); err != nil {
+		return
+	}
+	cut, ok := skip(b, start-1)
+	if !ok {
+		return fmt.Errorf("less than %d lines", start)
+	}
+	if n == 0 {
+		return nil
+	}
+	tail, ok := skip(cut, n)
+	if !ok {
+		return fmt.Errorf("less than %d lines after line %d", n, start)
+	}
+	t := int64(len(b) - len(cut))
+	if err = f.Truncate(t); err != nil {
+		return
+	}
+	if len(tail) > 0 {
+		_, err = f.WriteAt(tail, t)
+	}
+	return
+}
+
+func skip(b []byte, n int) ([]byte, bool) {
+	for ; n > 0; n-- {
+		if len(b) == 0 {
+			return nil, false
+		}
+		x := bytes.IndexByte(b, '\n')
+		if x < 0 {
+			x = len(b)
+		} else {
+			x++
+		}
+		b = b[x:]
+	}
+	return b, true
+}
