4faff39ebac0d123e685c74b755b6a0f3019754c
Ondrej Sika <ondrej@ondrejsika.com>
2021-12-18 08:32:21 +0100

    feat(cmd/digitalocean/all/list): Add support for listing all accounts

--- cmd/digitalocean/all/list/list.go
+++ cmd/digitalocean/all/list/list.go
@@ -1,11 +1,18 @@
 package list
 
 import (
+	"fmt"
+	"log"
+
 	parent_cmd "github.com/sikalabs/slu/cmd/digitalocean/all"
+	"github.com/sikalabs/slu/config"
+	"github.com/sikalabs/slu/utils/config_utils"
 	"github.com/sikalabs/slu/utils/digitalocean_utils"
 
 	"github.com/spf13/cobra"
 )
+
+var FlagAllAccounts bool
 
 var Cmd = &cobra.Command{
 	Use:   "list",
@@ -23,11 +30,34 @@ Currently, all resources means:
 `,
 	Args: cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		token := digitalocean_utils.GetToken()
-		digitalocean_utils.ListAll(token)
+		s := config.ReadSecrets()
+		// co := config.ReadConfig()
+		if FlagAllAccounts {
+			for _, do := range s.DigitalOcean {
+				fmt.Printf("===== Account: %s =====\n", do.Alias)
+				digitalocean_utils.ListAll(do.Token)
+				fmt.Println("")
+			}
+		} else {
+			// Show single account (from context)
+			do := config_utils.GetCurrentDigitalOceanAccount()
+			if do == nil {
+				log.Fatal("No context found")
+			}
+			fmt.Printf("===== Account: %s =====\n", do.Alias)
+			digitalocean_utils.ListAll(do.Token)
+			fmt.Println("")
+		}
 	},
 }
 
 func init() {
 	parent_cmd.Cmd.AddCommand(Cmd)
+	Cmd.PersistentFlags().BoolVarP(
+		&FlagAllAccounts,
+		"all",
+		"A",
+		false,
+		"List resources from all accounts",
+	)
 }
