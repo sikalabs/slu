b16ab238415537d6767c7f3ff4746b68ffb4ebf8
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-28 20:32:06 +0100

    fix(wireguard_utils): Fix wrong order of keys (swap preshared & public keys)

--- utils/wireguard_utils/wireguard_utils.go
+++ utils/wireguard_utils/wireguard_utils.go
@@ -15,7 +15,7 @@ func NewKeys() (string, string, string, error) {
 }
 
 func PrintNewKeys() {
-	priv, preshared, pub, _ := NewKeys()
+	priv, pub, preshared, _ := NewKeys()
 	fmt.Printf("priv:\t%s\n", priv)
 	fmt.Printf("publ:\t%s\n", pub)
 	fmt.Printf("pres:\t%s\n", preshared)
