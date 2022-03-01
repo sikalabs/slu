05898f18e993744e0cf8babdd59eac70c248bc62
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-01 13:01:26 +0100

    feat(lib/metrics_generator): Add metric generator server with default data

---
+++ lib/metrics_generator/server/server.go
@@ -0,0 +1,144 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sikalabs/slu/version"
+)
+
+var promInfo = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "example_info",
+	Help: "Instance info",
+	ConstLabels: prometheus.Labels{
+		"version": version.Version,
+		"cmd0":    "slu",
+		"cmd1":    "promdemo",
+	},
+})
+
+var promRequestDurationSeconds = prometheus.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Namespace: "example",
+		Name:      "request_duration_seconds",
+		Help:      "Request duration in seconds",
+		Buckets:   []float64{.01, .05, .1, .2, .5, 1, 2, 5},
+	},
+	[]string{"status_code", "path", "method"},
+)
+
+type ServerStateMetrics struct {
+	StatusCode            int
+	Path                  string
+	Method                string
+	Duration              float64
+	DurationDeviationPerc int
+	Rate                  int
+	RateDeviationPerc     int
+}
+
+type ServerState struct {
+	Metrics []ServerStateMetrics
+}
+
+var State ServerState
+
+func getSleepTime(rate int) time.Duration {
+	return time.Duration(time.Duration(1000./float64(rate)) * time.Millisecond)
+}
+
+func addDeviationPerc(n float64, d int) float64 {
+	r := float64(-d/2+rand.Intn(d)) / 100.
+	return n + n*r
+}
+
+func runMetrics() {
+	window := 60
+	for {
+		for _, metric := range State.Metrics {
+			go func(m ServerStateMetrics) {
+				rate := int(addDeviationPerc(float64(window*m.Rate), m.RateDeviationPerc))
+				for i := 0; i < rate; i++ {
+					promRequestDurationSeconds.WithLabelValues(
+						strconv.Itoa(m.StatusCode),
+						m.Path,
+						m.Method,
+					).Observe(addDeviationPerc(m.Duration, m.DurationDeviationPerc))
+					time.Sleep(getSleepTime(m.Rate))
+				}
+			}(metric)
+		}
+		time.Sleep(time.Duration(window) * time.Second)
+	}
+}
+
+func Server() {
+	prometheus.MustRegister(promRequestDurationSeconds)
+	prometheus.MustRegister(promInfo)
+	promInfo.Set(1)
+
+	State = ServerState{
+		Metrics: []ServerStateMetrics{
+			{
+				StatusCode:            200,
+				Path:                  "/foo",
+				Method:                "GET",
+				Duration:              0.3,
+				DurationDeviationPerc: 10,
+				Rate:                  10,
+				RateDeviationPerc:     20,
+			},
+			{
+				StatusCode:            200,
+				Path:                  "/bar",
+				Method:                "GET",
+				Duration:              0.1,
+				DurationDeviationPerc: 10,
+				Rate:                  20,
+				RateDeviationPerc:     40,
+			},
+			{
+				StatusCode:            200,
+				Path:                  "/baz",
+				Method:                "GET",
+				Duration:              0.7,
+				DurationDeviationPerc: 10,
+				Rate:                  11,
+				RateDeviationPerc:     10,
+			},
+			{
+				StatusCode:            502,
+				Path:                  "/baz",
+				Method:                "GET",
+				Duration:              0.01,
+				DurationDeviationPerc: 10,
+				Rate:                  1,
+				RateDeviationPerc:     10,
+			},
+		},
+	}
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{
+			"cmd0":    "slu",
+			"cmd1":    "promdemo",
+			"metrics": "/metrics",
+		})
+	})
+
+	go runMetrics()
+
+	fmt.Println("Server started.")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
