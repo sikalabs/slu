a29c397e6c21a558cc1084dd7be84d2e507194e1
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-15 18:44:15 +0100

    feat(cmd/ondrejsika/desktop_cleanup): Add personal desktop cleanup util

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -86,6 +86,7 @@ import (
 	_ "github.com/sikalabs/slu/cmd/mysql/list"
 	_ "github.com/sikalabs/slu/cmd/mysql/ping"
 	_ "github.com/sikalabs/slu/cmd/ondrejsika"
+	_ "github.com/sikalabs/slu/cmd/ondrejsika/desktop_cleanup"
 	_ "github.com/sikalabs/slu/cmd/postgres"
 	_ "github.com/sikalabs/slu/cmd/postgres/create"
 	_ "github.com/sikalabs/slu/cmd/postgres/drop"
---
+++ cmd/ondrejsika/desktop_cleanup/desktop_cleanup.go
@@ -0,0 +1,36 @@
+package desktop_cleanup
+
+import (
+	"log"
+
+	parentcmd "github.com/sikalabs/slu/cmd/ondrejsika"
+	"github.com/sikalabs/slu/utils/exec_utils"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "desktop-cleanup",
+	Short: "Clean up desktop",
+	Args:  cobra.NoArgs,
+	Run: func(c *cobra.Command, args []string) {
+		rm(".minikube/cache")
+		rm("./Library/Caches/Yarn/*")
+		rm("./Library/Caches/pip/*")
+		rm("./Library/Caches/go-build/*")
+	},
+}
+
+func init() {
+	parentcmd.Cmd.AddCommand(Cmd)
+}
+
+func sh(script string) {
+	err := exec_utils.ExecShHomeOut(script)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func rm(path string) {
+	sh("rm -rf " + path)
+}
