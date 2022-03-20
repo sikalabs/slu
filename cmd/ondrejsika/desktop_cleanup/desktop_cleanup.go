3633abc5b5f89abd5ad910d43cd8921288b5c272
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-20 16:55:56 +0100

    feat(cmd/ondrejsika/desktop_cleanup): Print what will be cleaned up & add sleep time

--- cmd/ondrejsika/desktop_cleanup/desktop_cleanup.go
+++ cmd/ondrejsika/desktop_cleanup/desktop_cleanup.go
@@ -1,7 +1,9 @@
 package desktop_cleanup
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	parentcmd "github.com/sikalabs/slu/cmd/ondrejsika"
 	"github.com/sikalabs/slu/utils/exec_utils"
@@ -12,25 +14,49 @@ var FlagYesGoBuildCache bool
 var FlagYesGoPkgModCache bool
 var FlagYesYarnCache bool
 
+var ListSh []string
+var ListRm []string
+
 var Cmd = &cobra.Command{
 	Use:   "desktop-cleanup",
 	Short: "Clean up desktop",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		sh("brew cleanup")
-		rm(".minikube/cache")
+		// Prepare cleanup script
+		registerSh("brew cleanup")
+		registerRm(".minikube/cache")
 		if FlagYesYarnCache {
-			rm("./Library/Caches/Yarn/*")
+			registerRm("./Library/Caches/Yarn/*")
 		}
-		rm("./Library/Caches/pip/*")
+		registerRm("./Library/Caches/pip/*")
 		if FlagYesGoBuildCache {
-			rm("./Library/Caches/go-build/*")
+			registerRm("./Library/Caches/go-build/*")
 		}
-		rm("./Library/Caches/Homebrew/downloads/*")
+		registerRm("./Library/Caches/Homebrew/downloads/*")
 		if FlagYesGoPkgModCache {
-			rm("./go/pkg/mod/cache/*")
+			registerRm("./go/pkg/mod/cache/*")
 		}
-		rm(".nvm/.cache/*")
+		registerRm(".nvm/.cache/*")
+
+		// Review cleanup script
+		for _, script := range ListSh {
+			fmt.Println(script)
+		}
+		for _, rmParam := range ListRm {
+			fmt.Println("rm -rf", rmParam)
+		}
+
+		// Wait
+		fmt.Println("Wait for 10 seconds... cancel using ctrl+c")
+		time.Sleep(10 * time.Second)
+
+		// Do cleanup
+		for _, script := range ListSh {
+			sh(script)
+		}
+		for _, rmParam := range ListRm {
+			rm(rmParam)
+		}
 	},
 }
 
@@ -54,6 +80,14 @@ func init() {
 		false,
 		"Cleanup Yarn cache (rm -rf  ~/Library/Caches/Yarn/*)",
 	)
+}
+
+func registerSh(s string) {
+	ListSh = append(ListSh, s)
+}
+
+func registerRm(rmParam string) {
+	ListRm = append(ListRm, rmParam)
 }
 
 func sh(script string) {
