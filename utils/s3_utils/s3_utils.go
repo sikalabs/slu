c7b86e983672ed8d0099eac5b9834d457f0b10ff
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-28 21:01:09 +0200

    feat(utils/s3_utils): Add Upload method

--- utils/s3_utils/s3_utils.go
+++ utils/s3_utils/s3_utils.go
@@ -1,13 +1,17 @@
 package s3_utils
 
 import (
+	"io"
 	"log"
+	"os"
 
 	aws_aws "github.com/aws/aws-sdk-go/aws"
 	aws_credentials "github.com/aws/aws-sdk-go/aws/credentials"
 	aws_session "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	aws_s3 "github.com/aws/aws-sdk-go/service/s3"
+	aws_s3manager "github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/cheggaaa/pb/v3"
 )
 
 func DeleteBucketWithObjects(
@@ -64,6 +68,88 @@ func DeleteBucketWithObjects(
 	svc.DeleteBucket(&aws_s3.DeleteBucketInput{
 		Bucket: aws_aws.String(bucket_name),
 	})
+
+	return nil
+}
+
+func Upload(
+	access_key string,
+	secret_key string,
+	region string,
+	endpoint string,
+	bucket_name string,
+	key string,
+	f io.ReadSeeker,
+) error {
+	return baseUpload(
+		access_key,
+		secret_key,
+		region,
+		endpoint,
+		bucket_name,
+		key,
+		f,
+		5,
+		1,
+	)
+}
+
+func baseUpload(
+	access_key string,
+	secret_key string,
+	region string,
+	endpoint string,
+	bucket_name string,
+	key string,
+	f io.ReadSeeker,
+	partSizeMB int,
+	concurrency int,
+) error {
+	awsConfig := aws_aws.Config{
+		Credentials: aws_credentials.NewStaticCredentials(
+			access_key,
+			secret_key,
+			"",
+		),
+	}
+	if region != "" {
+		awsConfig.Region = aws_aws.String(region)
+	}
+	if endpoint != "" {
+		awsConfig.Region = aws_aws.String(string("us-east-1"))
+		awsConfig.S3ForcePathStyle = aws_aws.Bool(true)
+		awsConfig.Endpoint = aws_aws.String(endpoint)
+	}
+	session, err := aws_session.NewSession(
+		&awsConfig,
+	)
+	if err != nil {
+		return err
+	}
+	uploader := aws_s3manager.NewUploader(session, func(u *aws_s3manager.Uploader) {
+		u.PartSize = int64(partSizeMB) * 1024 * 1024 // The minimum/default allowed part size is 5MB
+		u.Concurrency = concurrency                  // default is 5
+	})
+
+	size, _ := f.Seek(0, os.SEEK_END)
+	f.Seek(0, 0)
+
+	bar := pb.Full.Start64(size)
+
+	// create proxy reader
+	barReader := bar.NewProxyReader(f)
+
+	_, err = uploader.Upload(&aws_s3manager.UploadInput{
+		Bucket: aws_aws.String(bucket_name),
+		ACL:    aws_aws.String("private"),
+		Key:    aws_aws.String(key),
+		Body:   barReader,
+	})
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
 
 	return nil
 }
