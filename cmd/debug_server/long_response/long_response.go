4931263cc644584b528c61ddb765291824f6fa5f
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-14 09:11:49 +0200

    feat: Add more info after HTTP server started

--- cmd/debug_server/long_response/long_response.go
+++ cmd/debug_server/long_response/long_response.go
@@ -28,7 +28,7 @@ var Cmd = &cobra.Command{
 			time.Sleep(time.Duration(sleepTime) * time.Second)
 			fmt.Fprintf(w, "[slu-debug-server] Response after %ds! %s \n", sleepTime, hostname)
 		})
-		fmt.Println("Server started.")
+		fmt.Println("Server started on 0.0.0.0:8000, see http://127.0.0.1:8000")
 		http.ListenAndServe(":8000", nil)
 	},
 }
--- cmd/example_server/example_server.go
+++ cmd/example_server/example_server.go
@@ -19,7 +19,7 @@ var Cmd = &cobra.Command{
 			hostname, _ := os.Hostname()
 			fmt.Fprintf(w, "[slu] Example HTTP Server! %s \n", hostname)
 		})
-		fmt.Println("Server started.")
+		fmt.Println("Server started on 0.0.0.0:8000, see http://127.0.0.1:8000")
 		http.ListenAndServe(":8000", nil)
 	},
 }
