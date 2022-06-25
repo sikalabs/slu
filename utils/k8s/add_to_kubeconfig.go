f7e9d99ff4ba878f6ce7f3bcd2bb63146ed855d9
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-25 16:15:09 +0200

    featI(utils/k8s/add_to_kubeconfig): Add chmod 600 for kubeconfig

--- utils/k8s/add_to_kubeconfig.go
+++ utils/k8s/add_to_kubeconfig.go
@@ -6,5 +6,6 @@ func AddToKubeconfigShell(path string) error {
 	return exec_utils.ExecOut("sh", "-c", `
 	cp ~/.kube/config ~/.kube/.config.$(date +%Y-%m-%d_%H-%M-%S).backup
 	KUBECONFIG=`+path+`:~/.kube/config kubectl config view --raw > /tmp/kubeconfig.merge.yml && cp /tmp/kubeconfig.merge.yml ~/.kube/config
+	chmod 600 ~/.kube/config
 	`)
 }
