cf8fc10674dad1dc20e30639a74a167d772a4dd5
Ondrej Sika <ondrej@ondrejsika.com>
2022-07-29 08:23:54 +0200

    feat(cmd/gitlab_ci/setup_runner): Add Gitlab URL & token param

--- cmd/gitlab_ci/setup_runner/setup_runner.go
+++ cmd/gitlab_ci/setup_runner/setup_runner.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var FlagGitlabUrl string
+var FlagRegistrationToken string
 var FlagGitlabName string
 var FlagConcurency int
 
@@ -19,14 +21,25 @@ var Cmd = &cobra.Command{
 	Short:   "Setup Gitlab Runner in Docker",
 	Aliases: []string{"sr"},
 	Run: func(c *cobra.Command, args []string) {
+		var err error
+
 		hostname, err := os.Hostname()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		gitlabUrl, registrationToken, err := vault_gitlab_ci.GetGitlabCiSecrets(FlagGitlabName)
-		if err != nil {
-			log.Fatal(err)
+		gitlabUrl := FlagGitlabUrl
+		registrationToken := FlagRegistrationToken
+
+		if FlagGitlabName != "" {
+			gitlabUrl, registrationToken, err = vault_gitlab_ci.GetGitlabCiSecrets(FlagGitlabName)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if gitlabUrl == "" {
+			log.Fatal("flags gitlab-url and registration-token OR flag gitlab (for Vault) are required")
 		}
 
 		err = setup_runner_utils.SetupGitlabRunnerDocker(gitlabUrl, registrationToken, hostname, FlagConcurency, false)
@@ -39,13 +52,28 @@ var Cmd = &cobra.Command{
 func init() {
 	gitlab_ci_cmd.Cmd.AddCommand(Cmd)
 	Cmd.PersistentFlags().StringVarP(
+		&FlagGitlabUrl,
+		"gitlab-url",
+		"u",
+		"",
+		"Gitlab URL",
+	)
+	Cmd.PersistentFlags().StringVarP(
+		&FlagRegistrationToken,
+		"registration-token",
+		"t",
+		"",
+		"Gitlab Registration Token",
+	)
+	Cmd.MarkFlagsRequiredTogether("gitlab-url", "registration-token")
+	Cmd.PersistentFlags().StringVarP(
 		&FlagGitlabName,
 		"gitlab",
 		"g",
 		"",
 		"Gitlab name in Vault (slu/gitlab-ci/$name)",
 	)
-	Cmd.MarkPersistentFlagRequired("gitlab")
+	Cmd.MarkFlagsMutuallyExclusive("gitlab-url", "gitlab")
 	Cmd.PersistentFlags().IntVarP(
 		&FlagConcurency,
 		"concurency",
