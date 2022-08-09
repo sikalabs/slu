e84c39659106fa3daaaea788b020e5f639c53149
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-09 15:37:08 +0200

    chore(install.sh): Create install script for oneliner installer

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VERSION=v0.1.0 && \
+OS=linux && \
+ARCH=amd64 && \
+BIN=install-slu && \
+curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+tar -xvzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
+rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+mv ${BIN} /usr/local/bin/ && \
+install-slu install
