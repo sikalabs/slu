b9be5436012e6b6b2554c9bd32d686ddd52918db
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-18 11:41:34 +0200

    feat(cmd/example_server): Add --port parameter to `slu example-server`

--- cmd/example_server/example_server.go
+++ cmd/example_server/example_server.go
@@ -4,26 +4,36 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/sikalabs/slu/cmd/root"
 	"github.com/spf13/cobra"
 )
+
+var FlagPort int
 
 var Cmd = &cobra.Command{
 	Use:   "example-server",
 	Short: "Run example web server",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-
+		portStr := strconv.Itoa(FlagPort)
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			hostname, _ := os.Hostname()
 			fmt.Fprintf(w, "[slu] Example HTTP Server! %s \n", hostname)
 		})
-		fmt.Println("Server started on 0.0.0.0:8000, see http://127.0.0.1:8000")
-		http.ListenAndServe(":8000", nil)
+		fmt.Println("Server started on 0.0.0.0:" + portStr + ", see http://127.0.0.1:" + portStr)
+		http.ListenAndServe(":"+portStr, nil)
 	},
 }
 
 func init() {
 	root.RootCmd.AddCommand(Cmd)
+	Cmd.PersistentFlags().IntVarP(
+		&FlagPort,
+		"port",
+		"p",
+		8000,
+		"Listen on port",
+	)
 }
