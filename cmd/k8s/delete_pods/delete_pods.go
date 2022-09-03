4f2470312b4f816f31c152caad503f8536bbcce4
Ondrej Sika <ondrej@ondrejsika.com>
2022-09-03 19:17:11 +0200

    feat(cmd/k8s/delete_pods): Add command to delete not ready pods

---
+++ cmd/k8s/delete_pods/delete_pods.go
@@ -0,0 +1,22 @@
+package delete_pods
+
+import (
+	k8s_cmd "github.com/sikalabs/slu/cmd/k8s"
+	"github.com/sikalabs/slu/utils/k8s"
+
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "delete-pods",
+	Short: "Delete \"not ready\" pods in all namespaces",
+	Args:  cobra.NoArgs,
+	Run: func(c *cobra.Command, args []string) {
+		clientset, _, _ := k8s.KubernetesClient()
+		k8s.DeleteNoReadyPods(clientset)
+	},
+}
+
+func init() {
+	k8s_cmd.Cmd.AddCommand(Cmd)
+}
