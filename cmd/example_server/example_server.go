618e426bcf4f665b60ed7195b63f24cd4ef0ab69
Ondrej Sika <ondrej@ondrejsika.com>
2022-11-14 23:12:08 +0100

    feat(example_server): Add /slow{1,10,30,60}s endpoints

--- cmd/example_server/example_server.go
+++ cmd/example_server/example_server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/sikalabs/slu/cmd/root"
 	"github.com/sikalabs/slu/version"
@@ -22,6 +23,22 @@ var Cmd = &cobra.Command{
 		hostname, _ := os.Hostname()
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "[slu "+version.Version+"] Example HTTP Server! %s %s \n", hostname, portStr)
+		})
+		http.HandleFunc("/slow1s", func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(1 * time.Second)
+			fmt.Fprintf(w, "[slu "+version.Version+"] Example HTTP Server (after 1s)! %s %s \n", hostname, portStr)
+		})
+		http.HandleFunc("/slow10s", func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(10 * time.Second)
+			fmt.Fprintf(w, "[slu "+version.Version+"] Example HTTP Server (after 10s)! %s %s \n", hostname, portStr)
+		})
+		http.HandleFunc("/slow30s", func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(30 * time.Second)
+			fmt.Fprintf(w, "[slu "+version.Version+"] Example HTTP Server (after 30s)! %s %s \n", hostname, portStr)
+		})
+		http.HandleFunc("/slow60s", func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(60 * time.Second)
+			fmt.Fprintf(w, "[slu "+version.Version+"] Example HTTP Server (after 60s)! %s %s \n", hostname, portStr)
 		})
 		fmt.Println("[slu " + version.Version + "] Server started on 0.0.0.0:" + portStr + ", see http://127.0.0.1:" + portStr)
 		http.ListenAndServe(":"+portStr, nil)
