c9a91bd22b2413ff6825019847a32c5325102ba5
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-11 16:42:45 +0100

    feat(digitalocean_utils): Add methods PrepareAllDomainsDelete, DoAllDomainsDelete

---
+++ utils/digitalocean_utils/delete_domains.go
@@ -0,0 +1,29 @@
+package digitalocean_utils
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/digitalocean/godo"
+)
+
+func PrepareAllDomainsDelete(token string) []godo.Domain {
+	client := godo.NewFromToken(token)
+	domains, _, _ := client.Domains.List(context.TODO(), &godo.ListOptions{})
+	fmt.Println("Domains marked for clean up:")
+	for _, el := range domains {
+		fmt.Println(el.Name)
+	}
+	return domains
+}
+
+func DoAllDomainsDelete(token string, Keys []godo.Domain) {
+	var err error
+	client := godo.NewFromToken(token)
+	for _, el := range Keys {
+		_, err = client.Domains.Delete(context.TODO(), el.Name)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
