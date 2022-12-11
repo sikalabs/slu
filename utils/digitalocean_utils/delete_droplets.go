dcae11344295db2a0505f02000032da46059bc22
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-11 16:30:36 +0100

    feat(digitalocean_utils): Add methods PrepareAllDropletsDelete, DoAllDropletsDelete

---
+++ utils/digitalocean_utils/delete_droplets.go
@@ -0,0 +1,29 @@
+package digitalocean_utils
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/digitalocean/godo"
+)
+
+func PrepareAllDropletsDelete(token string) []godo.Droplet {
+	client := godo.NewFromToken(token)
+	Droplets, _, _ := client.Droplets.List(context.TODO(), &godo.ListOptions{})
+	fmt.Println("Droplets marked for clean up:")
+	for _, v := range Droplets {
+		fmt.Println(v.Name)
+	}
+	return Droplets
+}
+
+func DoAllDropletsDelete(token string, DropletsForCleanUp []godo.Droplet) {
+	var err error
+	client := godo.NewFromToken(token)
+	for _, v := range DropletsForCleanUp {
+		_, err = client.Droplets.Delete(context.TODO(), v.ID)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
