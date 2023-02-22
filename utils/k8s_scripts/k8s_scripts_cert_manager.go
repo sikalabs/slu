0059e8442623d3c78281b61a2176e1b9ffa39d69
Ondrej Sika <ondrej@ondrejsika.com>
2023-02-22 21:08:48 +0100

    feat(utils/k8s_scripts/k8s_scripts_cert_manager): Add InstallClusterIssuerCloudflare

--- utils/k8s_scripts/k8s_scripts_cert_manager.go
+++ utils/k8s_scripts/k8s_scripts_cert_manager.go
@@ -28,3 +28,37 @@ spec:
             class: nginx
 EOF`, dry)
 }
+
+func InstallClusterIssuerCloudflare(
+	email string,
+	cloudflareToken string,
+	dry bool,
+) {
+	sh(`cat <<EOF | kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt
+spec:
+  acme:
+    email: `+email+`
+    server: https://acme-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-issuer-account-key
+    solvers:
+      - dns01:
+          cloudflare:
+            apiTokenSecretRef:
+              name: cloudflare-api-token
+              key: api-token
+EOF`, dry)
+	sh(`cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: cloudflare-api-token
+  namespace: cert-manager
+stringData:
+  api-token: `+cloudflareToken+`
+EOF`, dry)
+}
