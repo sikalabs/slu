6073809742ea944d6123456d5efc1b8b9e394a4a
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-29 11:28:31 +0200

    feat(cmd/install_bin): Add func InstallBinForExternalGoUse

---
+++ cmd/install_bin/external.go
@@ -0,0 +1,34 @@
+package install_bin
+
+func InstallBinForExternalGoUse(name string, os string, arch string, binDir string) {
+	for _, tool := range Tools {
+		if tool.Name != name {
+			continue
+		}
+
+		getOsFunc := func(x string) string { return x }
+		if tool.GetOsFunc != nil {
+			getOsFunc = tool.GetOsFunc
+		}
+
+		getArchFunc := func(x string) string { return x }
+		if tool.GetArchFunc != nil {
+			getArchFunc = tool.GetArchFunc
+		}
+
+		run(
+			tool.Name,
+			tool.Aliases,
+			tool.SourcePath,
+			tool.UrlTemplate,
+			tool.GetVersionFunc,
+			getUrl,
+			getSourcePath,
+			getOsFunc,
+			getArchFunc,
+			func() string { return os },
+			func() string { return arch },
+			func() string { return binDir },
+		)
+	}
+}
