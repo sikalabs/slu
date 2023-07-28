39c8a3bf675a776398bbd1238681d6fd37c12ae0
Ondrej Sika <ondrej@ondrejsika.com>
2023-07-28 15:05:48 +0900

    feat(cmd/k8s/delete_pods/delete_pods): Add --force param

--- cmd/k8s/delete_pods/delete_pods.go
+++ cmd/k8s/delete_pods/delete_pods.go
@@ -7,16 +7,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var FlagForce bool
+
 var Cmd = &cobra.Command{
 	Use:   "delete-pods",
 	Short: "Delete \"not ready\" pods in all namespaces",
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
 		clientset, _, _ := k8s.KubernetesClient()
-		k8s.DeleteNoReadyPods(clientset, false)
+		k8s.DeleteNoReadyPods(clientset, FlagForce)
 	},
 }
 
 func init() {
 	k8s_cmd.Cmd.AddCommand(Cmd)
+	Cmd.Flags().BoolVar(
+		&FlagForce,
+		"force",
+		false,
+		"Force delete pods (set grace period to 0)",
+	)
 }
