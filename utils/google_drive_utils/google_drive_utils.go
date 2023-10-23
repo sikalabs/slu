4c8530e0b3385350d80ffcd238b48a2fd7a897d0
Ondrej Sika <ondrej@ondrejsika.com>
2023-10-23 02:02:12 +0200

    feat(utils/google_drive_utils): Add GetToken method

--- utils/google_drive_utils/google_drive_utils.go
+++ utils/google_drive_utils/google_drive_utils.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"golang.org/x/oauth2"
 	drive "google.golang.org/api/drive/v3"
@@ -50,4 +51,35 @@ func Upload(clientId, clientSecret, accessToken, fileToUpload string) {
 	}
 
 	fmt.Printf("File ID: %s\n", file.Id)
+}
+
+func GetToken(clientId, clientSecret string) {
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		Scopes:       []string{"https://www.googleapis.com/auth/drive.file"},
+		Endpoint: oauth2.Endpoint{
+			TokenURL:      "https://oauth2.googleapis.com/token",
+			DeviceAuthURL: "https://oauth2.googleapis.com/device/code",
+		},
+	}
+
+	code, err := conf.DeviceAuth(ctx)
+	if err != nil {
+		log.Fatalf("Failed to get device and user codes: %v", err)
+	}
+
+	fmt.Printf("Visit the URL: %s\n", code.VerificationURI)
+	fmt.Printf("And enter the code: %s\n", code.UserCode)
+
+	for {
+		token, err := conf.DeviceAccessToken(ctx, code)
+		if err == nil {
+			fmt.Printf("Got access token: %s\n", token.AccessToken)
+			fmt.Printf("Got refresh token: %s\n", token.RefreshToken)
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
