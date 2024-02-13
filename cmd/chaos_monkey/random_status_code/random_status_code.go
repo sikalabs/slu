e3aae799a12d4c4d0f386efde53286ce30727ea6
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-13 08:20:57 +0100

    feat(cmd/chaos_monkey/random_status_code): Add --verbose flag

--- cmd/chaos_monkey/random_status_code/random_status_code.go
+++ cmd/chaos_monkey/random_status_code/random_status_code.go
@@ -1,12 +1,15 @@
 package random_status_code
 
 import (
+	"fmt"
 	"os"
 
 	parent_cmd "github.com/sikalabs/slu/cmd/chaos_monkey"
 	"github.com/sikalabs/slu/utils/random_utils"
 	"github.com/spf13/cobra"
 )
+
+var FlagVerbose bool
 
 var Cmd = &cobra.Command{
 	Use:     "random-status-code",
@@ -15,8 +18,12 @@ var Cmd = &cobra.Command{
 	Run: func(c *cobra.Command, args []string) {
 		ok := random_utils.RandomBool()
 		if ok {
+			if FlagVerbose {
+				fmt.Println("exit code 0")
+			}
 			os.Exit(0)
 		} else {
+			fmt.Println("exit code 1")
 			os.Exit(1)
 		}
 	},
@@ -24,4 +31,11 @@ var Cmd = &cobra.Command{
 
 func init() {
 	parent_cmd.Cmd.AddCommand(Cmd)
+	Cmd.PersistentFlags().BoolVarP(
+		&FlagVerbose,
+		"verbose",
+		"v",
+		false,
+		"Verbose output",
+	)
 }
