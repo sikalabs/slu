2cb83070f347f95bb734df3fc08fb3e90b93440b
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-14 20:40:49 +0100

    fix(utils/k8s_scripts): Fix values for InstallArgoCDDomain (with ingress)

--- utils/k8s_scripts/k8s_scripts_argocd.go
+++ utils/k8s_scripts/k8s_scripts_argocd.go
@@ -10,17 +10,17 @@ func InstallArgoCD(namespace string, dry bool) {
 }
 
 func InstallArgoCDDomain(namespace string, domain string, dry bool) {
+	// https://github.com/argoproj/argo-helm/blob/main/charts/argo-cd/values.yaml
 	sh(`helm upgrade --install \
 	argocd argo-cd \
 	--repo https://argoproj.github.io/argo-helm \
 	--create-namespace \
 	--namespace `+namespace+` \
 	--set 'server.ingress.enabled=true' \
-	--set 'server.ingress.hosts[0]='`+domain+` \
+	--set 'server.ingress.hostname='`+domain+` \
 	--set 'server.ingress.ingressClassName=nginx' \
 	--set 'server.ingress.annotations.cert-manager\.io/cluster-issuer=letsencrypt' \
 	--set 'server.ingress.annotations.nginx\.ingress\.kubernetes\.io/backend-protocol=HTTPS' \
-	--set 'server.ingress.tls[0].hosts[0]=`+domain+`' \
-	--set 'server.ingress.tls[0].secretName=argocd-tls' \
+	--set 'server.ingress.tls=true' \
 	--wait`, dry)
 }
