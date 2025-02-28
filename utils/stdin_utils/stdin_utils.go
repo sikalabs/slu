f84eb4c05046933c62ff410f5a31f98f397d7357
Ondrej Sika <ondrej@ondrejsika.com>
2025-02-28 15:41:22 +0100

    feat(utils/stdin_utils): Create method ReadFromPipeOrDie() string

--- utils/stdin_utils/stdin_utils.go
+++ utils/stdin_utils/stdin_utils.go
@@ -2,6 +2,7 @@ package stdin_utils
 
 import (
 	"bufio"
+	"log"
 	"os"
 )
 
@@ -16,4 +17,26 @@ func ReadAll() string {
 		all = all + text
 	}
 	return all
+}
+
+func ReadFromPipeOrDie() string {
+	var jwtToken string
+
+	// Ensure input is from a pipe
+	fi, err := os.Stdin.Stat()
+	if err != nil || fi.Mode()&os.ModeNamedPipe == 0 {
+		log.Fatalln("No input from pipe.")
+	}
+
+	// Read the input from stdin (pipe)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		jwtToken = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error reading from stdin: ", err)
+	}
+
+	return jwtToken
 }
