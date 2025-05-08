c98401951931e556043326c72c025d503e3892c7
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-08 12:42:23 +0200

    feat(telegram_utils): Add func TelegramSendMessage

--- utils/telegram_utils/telegram_utils.go
+++ utils/telegram_utils/telegram_utils.go
@@ -1,11 +1,31 @@
 package telegram_utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+func TelegramSendMessage(botToken string, chatID int64, message string) error {
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+	body, err := json.Marshal(map[string]string{
+		"chat_id":    fmt.Sprintf("%d", chatID),
+		"text":       message,
+		"parse_mode": "MarkdownV2",
+	})
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
 
 func TelegramGetLastChatID(botToken string) (int64, error) {
 	type Chat struct {
