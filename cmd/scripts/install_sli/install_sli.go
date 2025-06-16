1ac7018ae69ae4ae61113f749661ca9690682981
Ondrej Sika <ondrej@ondrejsika.com>
2025-06-16 19:45:10 +0200

    feat(cmd/scripts/install_sli): Add slu s install-sli (sikalabs internal utils)

---
+++ cmd/scripts/install_sli/install_sli.go
@@ -0,0 +1,93 @@
+package install_sli
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"syscall"
+
+	parent_cmd "github.com/sikalabs/slu/cmd/scripts"
+	"github.com/sikalabs/slu/utils/tar_gz_utils"
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+)
+
+var Cmd = &cobra.Command{
+	Use:     "install-sli",
+	Short:   "install sli - sikalabs internal utils",
+	Aliases: []string{"isli"},
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Print("Enter password: ")
+		bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println() // Print a newline after password input
+
+		installSli(string(bytePassword))
+
+		fmt.Println("sli installed successfully to current folder! You can try it with `./sli` command.")
+	},
+}
+
+func init() {
+	parent_cmd.Cmd.AddCommand(Cmd)
+}
+
+func installSli(password string) {
+	const (
+		encryptedToken              = "eqLoHqyn0Sq9t+SRafoI/XJKA4ePdUn4DylKVfn2tQMvG06WxRyAN6Bdj90aCQ5VX4/E6t+jHOB1awwnfDX1SNISAeYk7bAL5+2whjL++kUxQMAHKELZt+hegcRYOGFQcpDLnfXrXlELu+Ox4CK1ttUXjAi9f+Xdew=="
+		encryptedAssetIDDarwinArm64 = "ZeX1YA38SMaPx/JlCpwBYxwWeRYPcyg5yXU28hhvIhttqLJopQ=="
+	)
+
+	tar_gz_utils.WebTarGzToBin(
+		"https://api.github.com/repos/sikalabs/sli/releases/assets/"+decrypt(encryptedAssetIDDarwinArm64, password),
+		"sli",
+		map[string]string{
+			"Accept":        "application/octet-stream",
+			"Authorization": "Bearer " + decrypt(encryptedToken, password),
+		},
+		"./sli",
+	)
+}
+
+func decrypt(encryptedDataBase64, password string) string {
+	key := deriveKey(password)
+
+	encryptedData, err := base64.StdEncoding.DecodeString(encryptedDataBase64)
+	if err != nil {
+		log.Fatalf("Failed to decode encrypted data: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("Failed to create cipher: %v", err)
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalf("Failed to create GCM: %v", err)
+	}
+
+	nonceSize := aesGCM.NonceSize()
+	if len(encryptedData) < nonceSize {
+		log.Fatal("Ciphertext too short")
+	}
+
+	nonce, ciphertext := encryptedData[:nonceSize], encryptedData[nonceSize:]
+
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		log.Fatalf("Decryption failed: %v", err)
+	}
+
+	return string(plaintext)
+}
+
+func deriveKey(password string) []byte {
+	hash := sha256.Sum256([]byte(password))
+	return hash[:]
+}
