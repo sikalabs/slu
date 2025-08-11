69bf7142b588caab997a5a59ca682baa3641f980
Ondrej Sika <ondrej@ondrejsika.com>
2025-08-11 08:09:35 +0200

    WIP

--- cmd/kubelogin/kubelogin.go
+++ cmd/kubelogin/kubelogin.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"fmt"
 
 	"github.com/int128/kubelogin/pkg/di"
 	"github.com/sikalabs/slu/cmd/root"
@@ -21,10 +22,30 @@ var Cmd = &cobra.Command{
 		ctx := context.Background()
 		ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 		defer stop()
+		log(fmt.Sprintf("%s\n", os.Args))
+		log(fmt.Sprintf("%s\n", os.Args[1:]))
 		os.Exit(di.NewCmd().Run(ctx, os.Args[1:], version))
 	},
 }
 
 func init() {
 	root.RootCmd.AddCommand(Cmd)
+}
+
+func log(line string) {
+	filePath := "example.log"
+
+	// Open the file in append mode, create if it doesn't exist
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer f.Close()
+
+	// Write the line
+	if _, err := f.WriteString(line); err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
 }
