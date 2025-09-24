474f56a2fef7975e4ff15a21a1ec3c94c395369f
Ondrej Sika <ondrej@ondrejsika.com>
2025-09-24 07:23:47 +0200

    feat(cmd/serve_files): Add flag -p for port selection

--- cmd/serve_files/serve_files.go
+++ cmd/serve_files/serve_files.go
@@ -6,16 +6,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var FlagPort int
+
 var Cmd = &cobra.Command{
 	Use:     "serve-files",
 	Aliases: []string{"serve"},
 	Short:   "Serve files in local directory",
 	Args:    cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		file_server.Server(8000)
+		file_server.Server(FlagPort)
 	},
 }
 
 func init() {
 	root.RootCmd.AddCommand(Cmd)
+	Cmd.PersistentFlags().IntVarP(
+		&FlagPort,
+		"port",
+		"p",
+		8000,
+		"Listen on port",
+	)
 }
