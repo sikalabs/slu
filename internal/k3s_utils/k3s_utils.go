43d5af26eb72f8b672b3362072d2d749dcb329d5
Ondrej Sika <ondrej@ondrejsika.com>
2025-10-09 23:09:46 +0200

    feat(internal/k3s_utils): Add func CheckIfKubectlIsLinkOfK3s

---
+++ internal/k3s_utils/k3s_utils.go
@@ -0,0 +1,40 @@
+package k3s_utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func CheckIfKubectlIsLinkOfK3s() bool {
+	kubectl := "/usr/local/bin/kubectl"
+	k3s := "/usr/local/bin/k3s"
+
+	info, err := os.Lstat(kubectl)
+	if err != nil {
+		return false
+	}
+
+	if info.Mode()&os.ModeSymlink != 0 {
+		target, err := os.Readlink(kubectl)
+		if err != nil {
+			return false
+		}
+
+		// Resolve both to absolute paths
+		resolvedkubectl, err := filepath.EvalSymlinks(kubectl)
+		if err != nil {
+			return false
+		}
+		resolvedk3s, err := filepath.EvalSymlinks(k3s)
+		if err != nil {
+			return false
+		}
+
+		fmt.Println("kubectl points to:", target)
+		if resolvedkubectl == resolvedk3s {
+			return true
+		}
+	}
+	return false
+}
