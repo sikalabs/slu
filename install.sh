4a35e5a61901b8e942f2c12458b83f0d18e1d0d6
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-04 00:23:23 +0200

    feat(install.sh): Dont use install-slu

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VERSION=v0.1.0 && \
+VERSION=v0.100.0 && \
 OS=linux && \
 ARCH=amd64 && \
-BIN=install-slu && \
+BIN=slu && \
 curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
 tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
 rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
-mv ${BIN} /usr/local/bin/ && \
-/usr/local/bin/install-slu install
+mv ${BIN} /usr/local/bin/slu && \
+/usr/local/bin/slu install-bin slu
